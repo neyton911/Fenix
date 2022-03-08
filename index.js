8b1238c32a5f263ac7d30e6b9a48a8e0a239885b
neyton911 <dok2014.naigre@mail.ru>
2022-03-08 06:32:35 +0600

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,13 @@
+do_a();
+if (check) {
+    something();
+} else {
+    anything();
+    if (whatever()) {
+        for (a in b) {
+            if (good(a)) {
+            something();
+            }
+        }
+    }
+}
